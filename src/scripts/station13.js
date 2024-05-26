737eb670bf0c0335878220e4167d544429b904d2
takumi.takahashi <takumi.takahashi@gpol.co.jp>
2024-05-26 20:43:43 +0900

    [update]front-railway2

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,12 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
+  let flag = false;
+  const element = document.getElementById('check');
+  const txt = document.getElementById('text');
+  if(element.checked === true){
+    txt.style.backgroundColor = 'red';
+  } else {
+    txt.style.backgroundColor = 'transparent';
+  }
 
 }
---
+++ src/scripts/station14.js
@@ -0,0 +1,3 @@
+function alertPopup(){
+  alert("ECMAScript");
+}
